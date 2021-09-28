a0c9b3e6374c743d6fdafe9a08900f03fe647784
fmis13 <76878393+fmis13@users.noreply.github.com>
2021-09-28 21:50:37 +0200

    Delete password-protected-site directory

--- password-protected-site/admin/scripts/cookie-checker.js
+++
@@ -1,3 +0,0 @@
-if (document.cookie.indexOf("AdminPortal=Valid") == -1) {
-        location.href = "../index.html"; //Where they will be taken if they do not have the proper authentication
-}
--- password-protected-site/scripts/cookie-checker.js
+++
@@ -1,3 +0,0 @@
-if (document.cookie.indexOf("SiteSession=Valid") == -1) {
-        location.href = "login/index.html";
-    }
